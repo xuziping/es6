e77613cbe1ba983c8b655051db505ea75e44beff
Kevin Xu <kxu@campuscruiser.com>
2017-06-12 14:34:10 +0800

    for tasks

--- tasks/scripts.js
+++ tasks/scripts.js
@@ -2,7 +2,7 @@ import gulp from 'gulp';
 import gulpif from 'gulp-if';
 import concat from 'gulp-concat';
 import webpack from 'webpack';
-import gulpWebpack from 'webpack-stram';
+import gulpWebpack from 'webpack-stream';
 import named from 'vinyl-named';
 import livereload from 'gulp-livereload';
 import plumber from 'gulp-plumber';
